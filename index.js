e94e1234959dba3f15ef3d3ae8bc69ae2dbae913
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-22 21:29:11 +0530

    changed to true

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use((req, res, next) => {
 //     next()
 //   })
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: false }))
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 
 app.post("/api/upload", async (req, res) => {
