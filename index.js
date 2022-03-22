84a054c610256a15a0c9b4deda7cf358bb0e32ac
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-22 14:53:44 +0530

    cors added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { cloudinary } = require("./utils/cloudinary");
 
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
