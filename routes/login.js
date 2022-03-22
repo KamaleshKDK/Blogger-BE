6983a22c6e432d64e8a484e3fda7ac04907ebd2e
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-22 15:43:21 +0530

    email to username

--- routes/login.js
+++ routes/login.js
@@ -29,16 +29,16 @@ router.post("/login", async (req, res) => {
         if (error)
             return res.status(400).send({ message: error.details[0].message });
 
-        const user = await User.findOne({ email: req.body.email });
+        const user = await User.findOne({ username: req.body.username });
         if (!user)
-            return res.status(401).send({ message: "Invalid Email or Password" });
+            return res.status(401).send({ message: "Invalid Username or Password" });
 
         const validPassword = await bcrypt.compare(
             req.body.password,
             user.password
         );
         if (!validPassword)
-            return res.status(401).send({ message: "Invalid Email or Password" });
+            return res.status(401).send({ message: "Invalid Username or Password" });
 
         const token = user.generateAuthToken();
         res.status(200).send({ data: token, message: "logged in successfully" });
@@ -50,7 +50,7 @@ router.post("/login", async (req, res) => {
 
 const validate = (data) => {
     const schema = Joi.object({
-        email: Joi.string().email().required().label("Email"),
+        username: Joi.string().text().required().label("Username"),
         password: Joi.string().required().label("Password"),
     });
     return schema.validate(data);
